a0014811fc6c0a2fce032c3dba5066962426b485
Eclips4 <kirill.bast9@mail.ru>
2022-11-12 23:01:33 +0200

    Refactoring and add impl Iterator<Item = String>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
-use minigrep::{Config, run, search};
+use minigrep::{Config, run};
+
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     if let Err(e) = run(config) {
