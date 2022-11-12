464b7d98070508fc16b1fdaba213ad7c298d8e30
Eclips4 <kirill.bast9@mail.ru>
2022-11-12 14:51:06 +0200

    Add search

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 use std::process;
 use std::error::Error;
-use minigrep::{Config, run};
+use minigrep::{Config, run, search};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,8 +10,6 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
     if let Err(e) = run(config) {
         println!("Application error: {e}");
         process::exit(1);
